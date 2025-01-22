ce734407f7668603ec89d02b80a74f8eb7794b56
Khushkataruka <04khushkat@gmail.com>
2025-01-22 10:46:04 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ connect(url);
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173", //frontend url
+    origin: "https://real-time-chat-application-smoky.vercel.app", //frontend url
     credentials: true
 }))
 app.use(cookieParser()); // Use cookie-parser middleware
